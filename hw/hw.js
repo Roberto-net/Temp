2a69037181eb1acca4248be02f93196b4a2a8d5a
JustinReed713 <dmitriy.soloviov93@gmail.com>
2020-06-01 20:11:10 +0300

    css last

--- hw/hw.js
+++ hw/hw.js
@@ -1,13 +1,26 @@
-var coffeeMachine;
+var coffeeMachine = null;
 
 class CoffeeMachine {
     coffeeTank = 300;
     waterTank = 1000;
+    capIsFull = false;
     coffeeValue = document.getElementById("coffeeTankValue");
     screen = document.getElementById("msg");
     msg = ``;
     start() {
         this.screen.innerText = "Ready";
+    }
+    finish() {
+        this.coffeeTank = 0;
+        this.waterTank = 0;
+        if (this.capIsFull === true) {
+            this.drinkCoffee();
+            document.getElementById('capTank').addEventListener('transitionend', () => {
+                this.screen.innerText = "";
+            });
+        } else {
+            this.screen.innerText = "";
+        }
     }
     indicate() {
         const waterIndicator = document.getElementById("waterTank");
@@ -32,14 +45,22 @@ class CoffeeMachine {
         }
         this.coffeeTank -= 15;
         this.waterTank -= 100;
-        this.screen.innerText = `Your "Americano" was done`;
+        document.getElementById('capTank').addEventListener('transitionrun', () => {
+            if (this.capIsFull === true) {
+                this.screen.innerText = `You drinking your "Americano"`;
+            } else {
+                this.screen.innerText = `Create your "Americano"`;
+            }
+        })
+        document.getElementById('capTank').addEventListener('transitionend', () => {
+            this.screen.innerText = `Your "Americano" was done`;
+        })
         this.indicate();
-
+        this.fillCap();
     }
     espresso() {
         this.screen.innerText = ``;
         if (this.coffeeTank < 7) {
-
             this.screen.innerText = this.alertCoffee();
             return;
         }
@@ -49,8 +70,18 @@ class CoffeeMachine {
         }
         this.coffeeTank -= 7;
         this.waterTank -= 30;
-        this.screen.innerText = `Your "Espresso" was done`;
+        document.getElementById('capTank').addEventListener('transitionrun', () => {
+            if (this.capIsFull === true) {
+                this.screen.innerText = `You drinking your "Espresso"`;
+            } else {
+                this.screen.innerText = `Create your "Espresso"`;
+            }
+        })
+        document.getElementById('capTank').addEventListener('transitionend', () => {
+            this.screen.innerText = `Your "Espresso" was done`;
+        })
         this.indicate();
+        this.fillCap();
     }
     doubleespresso() {
         this.screen.innerText = ``;
@@ -64,9 +95,18 @@ class CoffeeMachine {
         }
         this.coffeeTank -= 15;
         this.waterTank -= 60;
-        this.screen.innerText = `Your "Double Espresso" was done`;
+        document.getElementById('capTank').addEventListener('transitionrun', () => {
+            if (this.capIsFull === true) {
+                this.screen.innerText = `You drinking your "Double Espresso"`;
+            } else {
+                this.screen.innerText = `Create your "Double Espresso"`;
+            }
+        })
+        document.getElementById('capTank').addEventListener('transitionend', () => {
+            this.screen.innerText = `Your "Double Espresso" was done`;
+        })
         this.indicate();
-
+        this.fillCap();
     }
     fillCoffee() {
         this.coffeeTank = 300;
@@ -84,28 +124,61 @@ class CoffeeMachine {
     alertWater() {
         return "Please fill water tank";
     }
+    fillCap() {
+        const cap = document.getElementById('capTank');
+        cap.style.boxShadow = `0px 0px 3px 3px gray, inset 0px ${Number.parseInt(window.getComputedStyle(cap).height) * 0.1}px 0px 0px aliceblue`;
+        cap.innerText = 'Drink me!';
+        cap.addEventListener('transitionstart', () => {
+            this.capIsFull = true;
+        })
+    }
+    drinkCoffee() {
+        const cap = document.getElementById('capTank');
+        cap.style.boxShadow = `0px 0px 3px 3px gray, inset 0px ${Number.parseInt(window.getComputedStyle(cap).height) + Number.parseInt(window.getComputedStyle(cap).paddingTop)}px 0px 0px aliceblue`;
+        cap.innerText = '';
+        cap.addEventListener('transitionend', () => {
+            this.capIsFull = false;
+            this.start();
+        })
+    }
 }
 document.getElementById("on-off").addEventListener('click', (event) => {
-    this.coffeeMachine = new CoffeeMachine();
-    this.coffeeMachine.start();
-    this.coffeeMachine.indicate();
+    if (coffeeMachine === null) {
+        coffeeMachine = new CoffeeMachine();
+        document.body.getElementsByClassName("iconStyle")[0].classList.add('iconStyleBlue');
+        coffeeMachine.start();
+        coffeeMachine.indicate();
+    } else {
+        document.body.getElementsByClassName("iconStyle")[0].classList.remove('iconStyleBlue');
+        coffeeMachine.finish();
+        coffeeMachine.indicate();
+        coffeeMachine = null;
+    }
 })
 
 document.getElementById("Americano").addEventListener('click', (event) => {
-    this.coffeeMachine.americano();
-
+    if (coffeeMachine.capIsFull === false) {
+        this.coffeeMachine.americano();
+    }
 })
 document.getElementById("Espresso").addEventListener('click', (event) => {
-    this.coffeeMachine.espresso();
+    if (coffeeMachine.capIsFull === false) {
+        this.coffeeMachine.espresso();
+    }
 })
 document.getElementById("DoubleEspresso").addEventListener('click', (event) => {
-    this.coffeeMachine.doubleespresso();
+    if (coffeeMachine.capIsFull === false) {
+        this.coffeeMachine.doubleespresso();
+    }
 })
 document.getElementById("fillWater").addEventListener('click', (event) => {
     this.coffeeMachine.fillWater();
 })
 document.getElementById("fillCoffee").addEventListener('click', (event) => {
     this.coffeeMachine.fillCoffee();
+})
+document.getElementById("capTank").addEventListener('click', (event) => {
+    this.coffeeMachine.drinkCoffee();
 })
 
 
