1f7f82152ce1609b8fc6124d7bae36f7d0cb3d5c
JustinReed713 <dmitriy.soloviov93@gmail.com>
2020-06-01 18:06:38 +0300

    css

--- hw/hw.js
+++ hw/hw.js
@@ -1,108 +1,111 @@
-var coffeMachine;
+var coffeeMachine;
 
-class CoffeMachine{
-    coffeTank=300;
-    waterTank=1000;
-    coffeValue=document.getElementById("coffeTankValue");
+class CoffeeMachine {
+    coffeeTank = 300;
+    waterTank = 1000;
+    coffeeValue = document.getElementById("coffeeTankValue");
     screen = document.getElementById("msg");
-    msg=``;
-    start(){
-        this.screen.innerText="Ready";
-        
+    msg = ``;
+    start() {
+        this.screen.innerText = "Ready";
     }
-    test(){
-        document.getElementById("waterTankValue").innerText=`${this.waterTank} ml`;
-        document.getElementById("coffeTankValue").innerText=`${this.coffeTank} gram`;
-        document.getElementById("coffeTank").style.boxShadow=`inset 0px -${(Number.parseInt(window.getComputedStyle(document.getElementById("coffeTank")).height)/300)*this.coffeTank}px 0px 0px rgb(77, 25, 25)`;
-        document.getElementById("waterTank").style.boxShadow=`inset 0px -${(Number.parseInt(window.getComputedStyle(document.getElementById("waterTank")).height)/1000)*this.waterTank}px 0px 0px rgb(49, 149, 243)`;
+    indicate() {
+        const waterIndicator = document.getElementById("waterTank");
+        const coffeeIndicator = document.getElementById("coffeeTank");
+        const waterTitle = document.getElementById("waterTankValue");
+        const coffeeTitle = document.getElementById("coffeeTankValue");
+        waterTitle.innerText = `${this.waterTank} ml`;
+        coffeeTitle.innerText = `${this.coffeeTank} gram`;
+        coffeeIndicator.style.boxShadow = `inset 0px -${(Number.parseInt(window.getComputedStyle(coffeeIndicator).height) / 300) * this.coffeeTank}px 0px 0px rgb(77, 25, 25)`;
+        waterIndicator.style.boxShadow = `inset 0px -${(Number.parseInt(window.getComputedStyle(waterIndicator).height) / 1000) * this.waterTank}px 0px 0px rgb(49, 149, 243)`;
     }
-    americano(){
+    americano() {
         console.log(this);
-        this.screen.innerText=``;
-        if(this.coffeTank<15){
-            this.screen.innerText=this.alertCoffe();
+        this.screen.innerText = ``;
+        if (this.coffeeTank < 15) {
+            this.screen.innerText = this.alertCoffee();
             return;
         }
-        if(this.waterTank<100){
-            this.screen.innerText=this.alertWater();
+        if (this.waterTank < 100) {
+            this.screen.innerText = this.alertWater();
             return;
         }
-        this.coffeTank-=15;
-        this.waterTank-=100;
-        this.screen.innerText=`Your "Americano" was done`;
-        this.test();
+        this.coffeeTank -= 15;
+        this.waterTank -= 100;
+        this.screen.innerText = `Your "Americano" was done`;
+        this.indicate();
 
     }
-    espresso(){
-        this.screen.innerText=``;
-        if(this.coffeTank<7){
-            
-            this.screen.innerText=this.alertCoffe();
+    espresso() {
+        this.screen.innerText = ``;
+        if (this.coffeeTank < 7) {
+
+            this.screen.innerText = this.alertCoffee();
             return;
         }
-        if(this.waterTank<30){
-            this.screen.innerText=this.alertWater();
+        if (this.waterTank < 30) {
+            this.screen.innerText = this.alertWater();
             return;
         }
-        this.coffeTank-=7;
-        this.waterTank-=30;
-        this.screen.innerText=`Your "Espresso" was done`;
-        this.test();
+        this.coffeeTank -= 7;
+        this.waterTank -= 30;
+        this.screen.innerText = `Your "Espresso" was done`;
+        this.indicate();
     }
-    doubleespresso(){
-        this.screen.innerText=``;
-        if(this.coffeTank<15){
-            this.screen.innerText=this.alertCoffe();
+    doubleespresso() {
+        this.screen.innerText = ``;
+        if (this.coffeeTank < 15) {
+            this.screen.innerText = this.alertCoffee();
             return;
         }
-        if(this.waterTank<60){
-            this.screen.innerText=this.alertWater();
+        if (this.waterTank < 60) {
+            this.screen.innerText = this.alertWater();
             return;
         }
-        this.coffeTank-=15;
-        this.waterTank-=60;
-        this.screen.innerText=`Your "Double Espresso" was done`;
-        this.test();
+        this.coffeeTank -= 15;
+        this.waterTank -= 60;
+        this.screen.innerText = `Your "Double Espresso" was done`;
+        this.indicate();
 
     }
-    fillCoffe(){
-        this.coffeTank=300;
-        this.screen.innerText=`Coffe tank was filled`;
-        this.test();
+    fillCoffee() {
+        this.coffeeTank = 300;
+        this.screen.innerText = `Coffee tank was filled`;
+        this.indicate();
     }
-    fillWater(){
-        this.waterTank=1000;
-        this.screen.innerText=`Water tank was filled`;
-        this.test();
+    fillWater() {
+        this.waterTank = 1000;
+        this.screen.innerText = `Water tank was filled`;
+        this.indicate();
     }
-    alertCoffe(){
-        return "Please fill coffe tank";
+    alertCoffee() {
+        return "Please fill coffee tank";
     }
-    alertWater(){
+    alertWater() {
         return "Please fill water tank";
     }
 }
-document.getElementById("on-off").addEventListener('click', (event)=>{
-    this.coffeMachine= new CoffeMachine();
-    this.coffeMachine.start();
-    this.coffeMachine.test();
+document.getElementById("on-off").addEventListener('click', (event) => {
+    this.coffeeMachine = new CoffeeMachine();
+    this.coffeeMachine.start();
+    this.coffeeMachine.indicate();
 })
 
-document.getElementById("Americano").addEventListener('click', (event)=>{
-    this.coffeMachine.americano();
-    
+document.getElementById("Americano").addEventListener('click', (event) => {
+    this.coffeeMachine.americano();
+
 })
-document.getElementById("Espresso").addEventListener('click', (event)=>{
-    this.coffeMachine.espresso();
+document.getElementById("Espresso").addEventListener('click', (event) => {
+    this.coffeeMachine.espresso();
 })
-document.getElementById("DoubleEspresso").addEventListener('click', (event)=>{
-    this.coffeMachine.doubleespresso();
+document.getElementById("DoubleEspresso").addEventListener('click', (event) => {
+    this.coffeeMachine.doubleespresso();
 })
-document.getElementById("fillWater").addEventListener('click', (event)=>{
-    this.coffeMachine.fillWater();
+document.getElementById("fillWater").addEventListener('click', (event) => {
+    this.coffeeMachine.fillWater();
 })
-document.getElementById("fillCoffe").addEventListener('click', (event)=>{
-    this.coffeMachine.fillCoffe();
+document.getElementById("fillCoffee").addEventListener('click', (event) => {
+    this.coffeeMachine.fillCoffee();
 })
 
 
